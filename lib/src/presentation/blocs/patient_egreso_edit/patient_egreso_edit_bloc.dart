c10ed5b5bac5792047e4b14ca8298cff7d1fd0ca
Carlos Bermudez <cbermudez.dev@gmail.com>
2021-09-19 15:55:14 -0400

    feat: implement egreso edit bloc logic

--- lib/src/presentation/blocs/patient_egreso_edit/patient_egreso_edit_bloc.dart
+++ lib/src/presentation/blocs/patient_egreso_edit/patient_egreso_edit_bloc.dart
@@ -1,4 +1,7 @@
 import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:egresocovid19/src/domain/entities/entities.dart';
+import 'package:egresocovid19/src/domain/repositories/repositories.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
 
@@ -14,11 +17,37 @@ abstract class IPatientEgresoEditBloc
 
 @Injectable(as: IPatientEgresoEditBloc)
 class PatientEgresoEditBloc extends IPatientEgresoEditBloc {
-  PatientEgresoEditBloc() : super(const PatientEgresoEditState.initial()) {
+  PatientEgresoEditBloc(
+    this.patientRepository,
+  ) : super(const PatientEgresoEditState.initial()) {
     on<PatientEgresoEditEvent>((event, emit) {
       event.when(
-        fetch: (patientId) {},
+        fetch: (patientId) async {
+          emit(const PatientEgresoEditState.fetchInProgress());
+
+          //TODO: Load actual patient discharge data
+          const Either<ErrorEntity, DischargeOfPositiveCasesOfCovid19Entity>
+              result = Right(DischargeOfPositiveCasesOfCovid19Entity());
+
+          result.fold(
+            (l) => emit(PatientEgresoEditState.fetchFailure(l.message)),
+            (r) => emit(PatientEgresoEditState.fetchSuccess(r)),
+          );
+        },
+        update: (patientId, newDischargeData) async {
+          emit(PatientEgresoEditState.updateInProgress(newDischargeData));
+
+          final result = await patientRepository.updateDischargeData(
+              patientId, newDischargeData);
+
+          result.fold(
+            (l) => emit(PatientEgresoEditState.updateFailure(l.message)),
+            (r) => emit(PatientEgresoEditState.updateSuccess(newDischargeData)),
+          );
+        },
       );
     });
   }
+
+  IPatientRepository patientRepository;
 }
--- lib/src/presentation/blocs/patient_egreso_edit/patient_egreso_edit_event.dart
+++ lib/src/presentation/blocs/patient_egreso_edit/patient_egreso_edit_event.dart
@@ -3,4 +3,9 @@ part of 'patient_egreso_edit_bloc.dart';
 @freezed
 class PatientEgresoEditEvent with _$PatientEgresoEditEvent {
   const factory PatientEgresoEditEvent.fetch(String patientId) = _Fetch;
+
+  const factory PatientEgresoEditEvent.update(
+    String patientId,
+    DischargeOfPositiveCasesOfCovid19Entity newDischargeData,
+  ) = _Update;
 }
