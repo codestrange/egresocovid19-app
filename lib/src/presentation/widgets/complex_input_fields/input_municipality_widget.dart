2c22475d2d706acfde7350d412bbc735e1b4d61e
Leonel Garcia <l.garcia3@estudiantes.matcom.uh.cu>
2021-09-19 02:58:40 -0400

    feat: Template for composed input widgets in patients

---
+++ lib/src/presentation/widgets/complex_input_fields/input_municipality_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class MunicipalityInputWidget extends StatelessWidget {
+  const MunicipalityInputWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(); //TODO: IMPLEMENT
+  }
+}
