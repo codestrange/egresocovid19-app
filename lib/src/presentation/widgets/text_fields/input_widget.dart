f935f14baf19aef9683c580d0e7b2b62c6bfee68
Leonel Garcia <l.garcia3@estudiantes.matcom.uh.cu>
2021-09-19 02:32:28 -0400

    fix: Improve text input base widget

--- lib/src/presentation/widgets/text_fields/input_widget.dart
+++ lib/src/presentation/widgets/text_fields/input_widget.dart
@@ -8,21 +8,30 @@ class TextInputWidget extends StatelessWidget {
     this.onChanged,
     this.errorText,
     this.hintText,
+    this.onSubmitted,
+    this.labelText,
+    this.keyboardType = TextInputType.text,
   }) : super(key: key);
 
   final TextEditingController? controller;
+  final TextInputType keyboardType;
   final ValueChanged<String>? onChanged;
+  final ValueChanged<String>? onSubmitted;
   final String? errorText;
   final String? hintText;
+  final String? labelText;
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       controller: controller,
+      keyboardType: keyboardType,
       onChanged: onChanged,
+      onSubmitted: onSubmitted,
       decoration: TextFieldDecorations.decoration(
         hintText: hintText,
         errorText: errorText,
+        labelText: labelText,
       ),
     );
   }
