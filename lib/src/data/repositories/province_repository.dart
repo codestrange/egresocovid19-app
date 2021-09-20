b0d8661fc070d2364a16fa5080acb91898222b4b
leynier <leynier41@gmail.com>
2021-09-20 03:11:08 -0400

    feat: implement province service

---
+++ lib/src/data/repositories/province_repository.dart
@@ -0,0 +1,26 @@
+import 'package:dartz/dartz.dart';
+import 'package:egresocovid19/src/data/data_sources/client_api.dart';
+import 'package:egresocovid19/src/data/extensions/extensions.dart';
+import 'package:egresocovid19/src/data/utils/utils.dart';
+import 'package:egresocovid19/src/domain/entities/entities.dart';
+import 'package:egresocovid19/src/domain/repositories/repositories.dart';
+import 'package:injectable/injectable.dart';
+
+@LazySingleton(as: IProvinceRepository)
+class ProvinceRepository implements IProvinceRepository {
+  const ProvinceRepository(
+    this.clientApi,
+  );
+
+  final ClientApi clientApi;
+
+  @override
+  Future<Either<ErrorEntity, List<ProvinceEntity>>> getProvinces() async {
+    try {
+      final response = await clientApi.getProvinces();
+      return Right(response.map((e) => e.toEntity()).toList());
+    } catch (e) {
+      return catchMethod(e);
+    }
+  }
+}
--- lib/src/domain/repositories/repositories.dart
+++ lib/src/domain/repositories/repositories.dart
@@ -1,4 +1,5 @@
 export 'auth_repository.dart';
 export 'locale_repository.dart';
 export 'patient_repository.dart';
+export 'province_repository.dart';
 export 'theme_repository.dart';
