550dd29f49154806d66d899b328cf52186e9d5ee
leynier <leynier41@gmail.com>
2021-09-20 03:19:42 -0400

    feat: implement autocomplete service

---
+++ lib/src/domain/repositories/autocomplete_repository.dart
@@ -0,0 +1,26 @@
+import 'package:dartz/dartz.dart';
+import 'package:egresocovid19/src/domain/entities/entities.dart';
+
+abstract class IAutoCompleteRepository {
+  Future<Either<ErrorEntity, List<String>>> getPolyclinics(String query);
+
+  Future<Either<ErrorEntity, List<String>>> getSurgeries(String query);
+
+  Future<Either<ErrorEntity, List<String>>> getPopularCouncils(String query);
+
+  Future<Either<ErrorEntity, List<String>>> getNeighborhoods(String query);
+
+  Future<Either<ErrorEntity, List<String>>> getDefaultPathologicals();
+
+  Future<Either<ErrorEntity, List<String>>> getAntibiotics(String query);
+
+  Future<Either<ErrorEntity, List<String>>> getAnotherVaccinesAgainstCovid(
+    String query,
+  );
+
+  Future<Either<ErrorEntity, List<String>>> getOthersAftermaths(String query);
+
+  Future<Either<ErrorEntity, List<String>>> getSymptoms(String query);
+
+  Future<Either<ErrorEntity, List<String>>> getDefaultSymptoms();
+}
--- lib/src/domain/services/services.dart
+++ lib/src/domain/services/services.dart
@@ -1,4 +1,5 @@
 export 'auth_service.dart';
+export 'autocomplete_service.dart';
 export 'locale_service.dart';
 export 'patient_service.dart';
 export 'province_service.dart';
