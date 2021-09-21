57959a524aa572f561ec07483b94a62cb9af26a9
Carlos Bermudez <cbermudez.dev@gmail.com>
2021-09-21 00:12:44 -0400

    feat: implement basic patient egreso edit page

--- lib/src/presentation/pages/patient_egreso_edit_page.dart
+++ lib/src/presentation/pages/patient_egreso_edit_page.dart
@@ -1,8 +1,12 @@
 import 'package:beamer/beamer.dart';
+import 'package:egresocovid19/src/domain/entities/entities.dart';
 import 'package:egresocovid19/src/presentation/blocs/blocs.dart';
+import 'package:egresocovid19/src/presentation/blocs/patient_egreso_edit_form/patient_egreso_edit_form_bloc.dart';
 import 'package:egresocovid19/src/presentation/utils/utils.dart';
+import 'package:egresocovid19/src/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_lyform/flutter_lyform.dart';
 import 'package:get_it/get_it.dart';
 
 class PatientEgresoEditPage extends StatelessWidget {
@@ -25,7 +29,9 @@ class PatientEgresoEditPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider<IPatientEgresoEditBloc>(
       create: (_) => GetIt.I()..add(PatientEgresoEditEvent.fetch(patientId)),
-      child: const _PatientEgresoEditPageInternal(),
+      child: _PatientEgresoEditPageInternal(
+        patientId: patientId,
+      ),
     );
   }
 }
@@ -33,10 +39,445 @@ class PatientEgresoEditPage extends StatelessWidget {
 class _PatientEgresoEditPageInternal extends StatelessWidget {
   const _PatientEgresoEditPageInternal({
     Key? key,
+    required this.patientId,
   }) : super(key: key);
+
+  final String patientId;
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            'Editar información de Egreso',
+            style: Theme.of(context).textTheme.headline1,
+          ),
+        ),
+      ),
+      body: BlocConsumer<IPatientEgresoEditBloc, PatientEgresoEditState>(
+        listener: (context, state) => state.when(
+          initial: () => null,
+          fetchInProgress: () => null,
+          fetchFailure: (errorMessage) => null,
+          fetchSuccess: (actualDischargeData) => null,
+          updateInProgress: (newDischargeData) => null,
+          updateFailure: (errorMessage, _) =>
+              ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(
+                'Ocurrio un error al guardar los datos. Error: $errorMessage',
+              ),
+            ),
+          ),
+          updateSuccess: (newDischargeData) =>
+              ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text('Datos guardados satisfactoriamente.'),
+            ),
+          ),
+        ),
+        builder: (context, state) => state.when(
+          initial: () => buildLoading(context),
+          fetchInProgress: () => buildLoading(context),
+          fetchFailure: (errorMessage) => buildError(
+            context,
+            errorMessage,
+          ),
+          fetchSuccess: (actualDischargeData) =>
+              buildEditBody(context, actualDischargeData),
+          updateInProgress: (newDischargeData) => buildEditBody(
+            context,
+            newDischargeData,
+            loading: true,
+          ),
+          updateFailure: (_, newDischargeData) => buildEditBody(
+            context,
+            newDischargeData,
+          ),
+          updateSuccess: (newDischargeData) => buildEditBody(
+            context,
+            newDischargeData,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildLoading(BuildContext context) {
+    return const SingleChildScrollView(
+      child: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+
+  Widget buildError(BuildContext context, String errorMessage) {
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Ha ocurrido un error al cargar los datos: $errorMessage'),
+          const SizedBox(
+            height: 20,
+          ),
+          OutlinedButton(
+            onPressed: () => context.read<IPatientEgresoEditBloc>().add(
+                  PatientEgresoEditEvent.fetch(
+                    patientId,
+                  ),
+                ),
+            child: const Text('Reintentar'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildEditBody(
+    BuildContext context,
+    DischargeDataEntity actualDischargeData, {
+    bool loading = false,
+  }) {
+    return SingleChildScrollView(
+      child: BlocProvider<IPatientEgresoEditFormBloc>(
+        create: (context) => GetIt.I.get(
+          param1: actualDischargeData,
+        ),
+        child: _PatientEgresoEditForm(
+          patientId: patientId,
+        ),
+      ),
+    );
+  }
+}
+
+class _PatientEgresoEditForm extends StatelessWidget {
+  const _PatientEgresoEditForm({
+    Key? key,
+    required this.patientId,
+  }) : super(
+          key: key,
+        );
+
+  final String patientId;
+
+  @override
+  Widget build(BuildContext context) {
+    return FormBlocListener<IPatientEgresoEditFormBloc, DischargeDataEntity,
+        ErrorEntity>(
+      onSuccess: (data) => context.read<IPatientEgresoEditBloc>().add(
+            PatientEgresoEditEvent.update(
+              patientId,
+              data,
+            ),
+          ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Flexible(
+            child: _DetectionDateField(),
+          ),
+          const SizedBox(height: 10),
+          Flexible(
+            child: _SymptomsField(),
+          ),
+          const SizedBox(height: 10),
+          Flexible(
+            child: _DurationOfSymptomsField(),
+          ),
+          const SizedBox(height: 10),
+          Flexible(
+            child: _DiagnosisWayField(),
+          ),
+          const SizedBox(height: 10),
+          Flexible(
+            child: _TestUsedInDiagnosisField(),
+          ),
+          const SizedBox(height: 10),
+          Flexible(
+            child: _DaysFromSymptomsToDiagnosisField(),
+          ),
+          const SizedBox(height: 10),
+          Flexible(
+            child: _NumberPcrPerformedField(),
+          ),
+          const SizedBox(height: 10),
+          Flexible(
+            child: _TimeFromDiagnosisToNegativeOrDischargeField(),
+          ),
+          const SizedBox(height: 10),
+          Flexible(
+            child: _FormOfContagionField(),
+          ),
+          const SizedBox(height: 10),
+          Flexible(
+            child: _WasHePartOfAnEventField(),
+          ),
+          const SizedBox(height: 10),
+          Flexible(
+            child: _DidHeWorkInTheAttentionToPositiveCasesField(),
+          ),
+          const SizedBox(height: 10),
+          Flexible(
+            child: _HospitalizationTimeField(),
+          ),
+          const SizedBox(height: 10),
+          Flexible(
+            child: _IncomesField(),
+          ),
+          const SizedBox(height: 10),
+          Flexible(
+            child: _ContactsFirstLevelField(),
+          ),
+          const SizedBox(height: 10),
+          Flexible(
+            child: _ContactsFirstLevelPositivesField(),
+          ),
+          const SizedBox(height: 10),
+          Flexible(
+            child: _ContactsSecondLevelField(),
+          ),
+          const SizedBox(height: 10),
+          Flexible(
+            child: _ContactsSecondLevelPositivesField(),
+          ),
+          const SizedBox(height: 10),
+          Flexible(
+            child: _ContactsThirdLevelField(),
+          ),
+          const SizedBox(height: 10),
+          Flexible(
+            child: _ContactsThirdLevelPositivesField(),
+          ),
+          const SizedBox(height: 10),
+          Flexible(
+            child: _TreatmentsReceivedField(),
+          ),
+          const SizedBox(height: 10),
+          Flexible(
+            child: _AntibioticsField(),
+          ),
+          const SizedBox(height: 10),
+          Flexible(
+            child: _ProphylaxisField(),
+          ),
+          const SizedBox(height: 10),
+          Flexible(
+            child: _AnotherVaccineAgainstCovidField(),
+          ),
+          const SizedBox(height: 10),
+          Flexible(
+            child: _AftermathField(),
+          ),
+          const SizedBox(height: 10),
+          Flexible(
+            child: _OthersAftermathField(),
+          ),
+          const SubmmitButton<IPatientEgresoEditFormBloc>('Editar'),
+        ],
+      ),
+    );
+  }
+}
+
+class _OthersAftermathField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class _AftermathField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class _AnotherVaccineAgainstCovidField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class _ProphylaxisField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class _AntibioticsField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class _TreatmentsReceivedField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class _ContactsThirdLevelPositivesField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class _ContactsThirdLevelField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class _ContactsSecondLevelPositivesField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class _ContactsSecondLevelField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class _ContactsFirstLevelPositivesField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class _ContactsFirstLevelField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class _IncomesField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class _HospitalizationTimeField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class _DidHeWorkInTheAttentionToPositiveCasesField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class _WasHePartOfAnEventField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class _FormOfContagionField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class _TimeFromDiagnosisToNegativeOrDischargeField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class _NumberPcrPerformedField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class _DaysFromSymptomsToDiagnosisField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class _TestUsedInDiagnosisField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class _DiagnosisWayField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class _DurationOfSymptomsField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class _SymptomsField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class _DetectionDateField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
   }
 }
