a27fba05289146e6d4a75e2aee5386bd90986c6b
Carlos Bermudez <cbermudez.dev@gmail.com>
2021-09-20 23:23:26 -0400

    feat: implement patient discharge data edit form bloc

--- lib/src/presentation/blocs/patient_egreso_edit_form/patient_egreso_edit_form_bloc.dart
+++ lib/src/presentation/blocs/patient_egreso_edit_form/patient_egreso_edit_form_bloc.dart
@@ -1,9 +1,11 @@
 import 'package:egresocovid19/src/domain/entities/entities.dart';
 import 'package:egresocovid19/src/domain/enums/enums.dart';
+import 'package:egresocovid19/src/presentation/blocs/blocs.dart';
 import 'package:flutter_lyform/flutter_lyform.dart';
 import 'package:injectable/injectable.dart';
 
-abstract class IPatientEgresoEditFormBloc extends FormBloc<void, ErrorEntity> {
+abstract class IPatientEgresoEditFormBloc
+    extends FormBloc<DischargeDataEntity, ErrorEntity> {
   InputBloc<DateTime?> get detectionDate;
   InputBloc<List<String>?> get symptoms;
   InputBloc<int?> get durationOfSymptoms;
@@ -63,114 +65,230 @@ abstract class IPatientEgresoEditFormBloc extends FormBloc<void, ErrorEntity> {
 @Injectable(as: IPatientEgresoEditFormBloc)
 class PatientEgresoEditFormBloc extends IPatientEgresoEditFormBloc {
   @override
-  // TODO: implement aftermath
-  InputBloc<List<Aftermath>?> get aftermath => throw UnimplementedError();
+  InputBloc<List<Aftermath>?> get aftermath => InputBloc(
+        pureValue: [],
+        validators: [],
+        validationType: ValidationType.explicit,
+      );
 
   @override
-  // TODO: implement anotherVaccineAgainstCovid
-  InputBloc<String?> get anotherVaccineAgainstCovid =>
-      throw UnimplementedError();
+  InputBloc<String?> get anotherVaccineAgainstCovid => InputBloc(
+        pureValue: null,
+        validators: [],
+        validationType: ValidationType.explicit,
+      );
 
   @override
-  // TODO: implement antibiotics
-  InputBloc<List<String>?> get antibiotics => throw UnimplementedError();
+  InputBloc<List<String>?> get antibiotics => InputBloc(
+        pureValue: [],
+        validators: [],
+        validationType: ValidationType.explicit,
+      );
 
   @override
-  // TODO: implement contactsFirstLevel
-  InputBloc<int?> get contactsFirstLevel => throw UnimplementedError();
+  InputBloc<int?> get contactsFirstLevel => InputBloc(
+        pureValue: null,
+        validators: [
+          IntValidator.nonNegative,
+        ],
+        validationType: ValidationType.explicit,
+      );
 
   @override
-  // TODO: implement contactsFirstLevelPositives
-  InputBloc<int?> get contactsFirstLevelPositives => throw UnimplementedError();
+  InputBloc<int?> get contactsFirstLevelPositives => InputBloc(
+        pureValue: null,
+        validators: [
+          IntValidator.nonNegative,
+        ],
+        validationType: ValidationType.explicit,
+      );
 
   @override
-  // TODO: implement contactsSecondLevel
-  InputBloc<int?> get contactsSecondLevel => throw UnimplementedError();
+  InputBloc<int?> get contactsSecondLevel => InputBloc(
+        pureValue: null,
+        validators: [
+          IntValidator.nonNegative,
+        ],
+        validationType: ValidationType.explicit,
+      );
 
   @override
-  // TODO: implement contactsSecondLevelPositives
-  InputBloc<int?> get contactsSecondLevelPositives =>
-      throw UnimplementedError();
+  InputBloc<int?> get contactsSecondLevelPositives => InputBloc(
+        pureValue: null,
+        validators: [
+          IntValidator.nonNegative,
+        ],
+        validationType: ValidationType.explicit,
+      );
 
   @override
-  // TODO: implement contactsThirdLevel
-  InputBloc<int?> get contactsThirdLevel => throw UnimplementedError();
+  InputBloc<int?> get contactsThirdLevel => InputBloc(
+        pureValue: null,
+        validators: [
+          IntValidator.nonNegative,
+        ],
+        validationType: ValidationType.explicit,
+      );
 
   @override
-  // TODO: implement contactsThirdLevelPositives
-  InputBloc<int?> get contactsThirdLevelPositives => throw UnimplementedError();
+  InputBloc<int?> get contactsThirdLevelPositives => InputBloc(
+        pureValue: null,
+        validators: [
+          IntValidator.nonNegative,
+        ],
+        validationType: ValidationType.explicit,
+      );
 
   @override
-  // TODO: implement daysFromSymptomsToDiagnosis
-  InputBloc<int?> get daysFromSymptomsToDiagnosis => throw UnimplementedError();
+  InputBloc<int?> get daysFromSymptomsToDiagnosis => InputBloc(
+        pureValue: null,
+        validators: [
+          IntValidator.nonNegative,
+        ],
+        validationType: ValidationType.explicit,
+      );
 
   @override
-  // TODO: implement detectionDate
-  InputBloc<DateTime?> get detectionDate => throw UnimplementedError();
+  InputBloc<DateTime?> get detectionDate => InputBloc(
+        pureValue: null,
+        validators: [],
+        validationType: ValidationType.explicit,
+      );
 
   @override
-  // TODO: implement diagnosisWay
-  InputBloc<DiagnosisWay?> get diagnosisWay => throw UnimplementedError();
+  InputBloc<DiagnosisWay?> get diagnosisWay => InputBloc(
+        pureValue: null,
+        validators: [],
+        validationType: ValidationType.explicit,
+      );
 
   @override
-  // TODO: implement didHeWorkInTheAttentionToPositiveCases
-  InputBloc<bool?> get didHeWorkInTheAttentionToPositiveCases =>
-      throw UnimplementedError();
+  InputBloc<bool?> get didHeWorkInTheAttentionToPositiveCases => InputBloc(
+        pureValue: null,
+        validators: [],
+        validationType: ValidationType.explicit,
+      );
 
   @override
-  // TODO: implement durationOfSymptoms
-  InputBloc<int?> get durationOfSymptoms => throw UnimplementedError();
+  InputBloc<int?> get durationOfSymptoms => InputBloc(
+        pureValue: null,
+        validators: [
+          IntValidator.nonNegative,
+        ],
+        validationType: ValidationType.explicit,
+      );
 
   @override
-  // TODO: implement formOfContagion
-  InputBloc<Contagion?> get formOfContagion => throw UnimplementedError();
+  InputBloc<Contagion?> get formOfContagion => InputBloc(
+        pureValue: null,
+        validators: [],
+        validationType: ValidationType.explicit,
+      );
 
   @override
-  // TODO: implement hospitalizationTime
-  InputBloc<String?> get hospitalizationTime => throw UnimplementedError();
+  InputBloc<String?> get hospitalizationTime => InputBloc(
+        pureValue: null,
+        validators: [],
+        validationType: ValidationType.explicit,
+      );
 
   @override
-  // TODO: implement incomes
-  InputBloc<List<IncomeEntity>?> get incomes => throw UnimplementedError();
+  InputBloc<List<IncomeEntity>?> get incomes => InputBloc(
+        pureValue: null,
+        validators: [],
+        validationType: ValidationType.explicit,
+      );
 
   @override
-  // TODO: implement numberPcrPerformed
-  InputBloc<int?> get numberPcrPerformed => throw UnimplementedError();
+  InputBloc<int?> get numberPcrPerformed => InputBloc(
+        pureValue: null,
+        validators: [],
+        validationType: ValidationType.explicit,
+      );
 
   @override
-  Future<FormBlocState<void, ErrorEntity>> onSubmmit() {
-    // TODO: implement onSubmmit
-    throw UnimplementedError();
+  Future<FormBlocState<DischargeDataEntity, ErrorEntity>> onSubmmit() async {
+    return FormSuccessState(
+      DischargeDataEntity(
+        detectionDate: detectionDate.state.value,
+        symptoms: symptoms.state.value,
+        durationOfSymptoms: durationOfSymptoms.state.value,
+        diagnosisWay: diagnosisWay.state.value,
+        testUsedInDiagnosis: testUsedInDiagnosis.state.value,
+        daysFromSymptomsToDiagnosis: daysFromSymptomsToDiagnosis.state.value,
+        numberPcrPerformed: numberPcrPerformed.state.value,
+        timeFromDiagnosisToNegativeOrDischarge:
+            timeFromDiagnosisToNegativeOrDischarge.state.value,
+        formOfContagion: formOfContagion.state.value,
+        wasHePartOfAnEvent: wasHePartOfAnEvent.state.value,
+        didHeWorkInTheAttentionToPositiveCases:
+            didHeWorkInTheAttentionToPositiveCases.state.value,
+        hospitalizationTime: hospitalizationTime.state.value,
+        incomes: incomes.state.value,
+        contactsFirstLevel: contactsFirstLevel.state.value,
+        contactsFirstLevelPositives: contactsFirstLevelPositives.state.value,
+        contactsSecondLevel: contactsSecondLevel.state.value,
+        contactsSecondLevelPositives: contactsSecondLevelPositives.state.value,
+        contactsThirdLevel: contactsThirdLevel.state.value,
+        contactsThirdLevelPositives: contactsThirdLevelPositives.state.value,
+        treatmentsReceived: treatmentsReceived.state.value,
+        antibiotics: antibiotics.state.value,
+        prophylaxis: prophylaxis.state.value,
+        anotherVaccineAgainstCovid: anotherVaccineAgainstCovid.state.value,
+        aftermath: aftermath.state.value,
+        othersAftermath: othersAftermath.state.value,
+      ),
+    );
   }
 
   @override
-  // TODO: implement othersAftermath
-  InputBloc<List<String>?> get othersAftermath => throw UnimplementedError();
+  InputBloc<List<String>?> get othersAftermath => InputBloc(
+        pureValue: null,
+        validators: [],
+        validationType: ValidationType.explicit,
+      );
 
   @override
-  // TODO: implement prophylaxis
-  InputBloc<List<Prophylaxis>?> get prophylaxis => throw UnimplementedError();
+  InputBloc<List<Prophylaxis>?> get prophylaxis => InputBloc(
+        pureValue: null,
+        validators: [],
+        validationType: ValidationType.explicit,
+      );
 
   @override
-  // TODO: implement symptoms
-  InputBloc<List<String>?> get symptoms => throw UnimplementedError();
+  InputBloc<List<String>?> get symptoms => InputBloc(
+        pureValue: null,
+        validators: [],
+        validationType: ValidationType.explicit,
+      );
 
   @override
-  // TODO: implement testUsedInDiagnosis
-  InputBloc<TestDiagnosis?> get testUsedInDiagnosis =>
-      throw UnimplementedError();
+  InputBloc<TestDiagnosis?> get testUsedInDiagnosis => InputBloc(
+        pureValue: null,
+        validators: [],
+        validationType: ValidationType.explicit,
+      );
 
   @override
-  // TODO: implement timeFromDiagnosisToNegativeOrDischarge
-  InputBloc<int?> get timeFromDiagnosisToNegativeOrDischarge =>
-      throw UnimplementedError();
+  InputBloc<int?> get timeFromDiagnosisToNegativeOrDischarge => InputBloc(
+        pureValue: null,
+        validators: [
+          IntValidator.nonNegative,
+        ],
+        validationType: ValidationType.explicit,
+      );
 
   @override
-  // TODO: implement treatmentsReceived
-  InputBloc<List<Treatment>?> get treatmentsReceived =>
-      throw UnimplementedError();
+  InputBloc<List<Treatment>?> get treatmentsReceived => InputBloc(
+        pureValue: null,
+        validators: [],
+        validationType: ValidationType.explicit,
+      );
 
   @override
-  // TODO: implement wasHePartOfAnEvent
-  InputBloc<bool?> get wasHePartOfAnEvent => throw UnimplementedError();
+  InputBloc<bool?> get wasHePartOfAnEvent => InputBloc(
+        pureValue: null,
+        validators: [],
+        validationType: ValidationType.explicit,
+      );
 }
