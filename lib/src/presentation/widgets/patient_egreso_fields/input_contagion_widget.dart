2492916571e2a0250938bdb1f7417c686034563b
Carlos Bermudez <cbermudez.dev@gmail.com>
2021-09-22 16:35:39 -0400

    feat: implement contagion input field

---
+++ lib/src/presentation/widgets/patient_egreso_fields/input_contagion_widget.dart
@@ -0,0 +1,44 @@
+import 'package:egresocovid19/src/domain/enums/enums.dart';
+import 'package:egresocovid19/src/presentation/widgets/text_fields/utils.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class ContagionInputWidget extends StatelessWidget {
+  const ContagionInputWidget({
+    Key? key,
+    this.onChanged,
+    this.onSubmitted,
+    this.errorText,
+    this.hintText,
+    this.labelText,
+  }) : super(key: key);
+
+  final ValueChanged<Contagion?>? onChanged;
+  final ValueChanged<Contagion?>? onSubmitted;
+  final String? errorText;
+  final String? hintText;
+  final String? labelText;
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButtonFormField<Contagion>(
+      items: Contagion.values.map((Contagion contagion) {
+        return DropdownMenuItem(
+          value: contagion,
+          child: Row(
+            children: <Widget>[
+              Text(contagion.toString()), //TODO: Localization
+            ],
+          ),
+        );
+      }).toList(),
+      onChanged: onChanged,
+      onSaved: onSubmitted,
+      decoration: TextFieldDecorations.decoration(
+        hintText: hintText,
+        errorText: errorText,
+        labelText: labelText,
+      ),
+    );
+  }
+}
