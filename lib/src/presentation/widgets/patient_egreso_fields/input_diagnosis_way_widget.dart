38ff53f7b0452468456dacbe4f4950c31478c8d0
Carlos Bermudez <cbermudez.dev@gmail.com>
2021-09-22 16:35:14 -0400

    feat: implement diagnosis way input field

---
+++ lib/src/presentation/widgets/patient_egreso_fields/input_diagnosis_way_widget.dart
@@ -0,0 +1,44 @@
+import 'package:egresocovid19/src/domain/enums/enums.dart';
+import 'package:egresocovid19/src/presentation/widgets/text_fields/utils.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class DiagnosisWayInputWidget extends StatelessWidget {
+  const DiagnosisWayInputWidget({
+    Key? key,
+    this.onChanged,
+    this.onSubmitted,
+    this.errorText,
+    this.hintText,
+    this.labelText,
+  }) : super(key: key);
+
+  final ValueChanged<DiagnosisWay?>? onChanged;
+  final ValueChanged<DiagnosisWay?>? onSubmitted;
+  final String? errorText;
+  final String? hintText;
+  final String? labelText;
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButtonFormField<DiagnosisWay>(
+      items: DiagnosisWay.values.map((DiagnosisWay diagnosisWay) {
+        return DropdownMenuItem(
+          value: diagnosisWay,
+          child: Row(
+            children: <Widget>[
+              Text(diagnosisWay.toString()), //TODO: Localization
+            ],
+          ),
+        );
+      }).toList(),
+      onChanged: onChanged,
+      onSaved: onSubmitted,
+      decoration: TextFieldDecorations.decoration(
+        hintText: hintText,
+        errorText: errorText,
+        labelText: labelText,
+      ),
+    );
+  }
+}
