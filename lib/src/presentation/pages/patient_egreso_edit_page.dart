baed290a82b7822a028dcf91c39dbd2e6744dbdd
Carlos Bermudez <cbermudez.dev@gmail.com>
2021-09-23 14:13:01 -0400

    fix: remove warnings

--- lib/src/presentation/pages/patient_egreso_edit_page.dart
+++ lib/src/presentation/pages/patient_egreso_edit_page.dart
@@ -10,7 +10,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_lyform/flutter_lyform.dart';
 import 'package:get_it/get_it.dart';
-import 'package:intl/intl.dart';
 
 class PatientEgresoEditPage extends StatelessWidget {
   const PatientEgresoEditPage({
