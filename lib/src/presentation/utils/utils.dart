cdfcf19534938126674bab70129aa686851ea720
leynier <leynier41@gmail.com>
2022-02-09 02:30:25 -0500

    feat: implement not validator

--- lib/src/presentation/utils/utils.dart
+++ lib/src/presentation/utils/utils.dart
@@ -2,4 +2,5 @@ export 'app_bloc_observer.dart';
 export 'custom_scroll_behavior.dart';
 export 'extensions.dart';
 export 'get_title.dart';
+export 'not_validator.dart';
 export 'translate_extension.dart';
